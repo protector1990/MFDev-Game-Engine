20ad9e74181b8676729d56c789be0ac19d9fff5d
marko.tanic <marko.tanic@cf8d60cf-3bd3-6f4a-91c3-f336ec15cb7d>
2016-03-13 18:37:59 +0000

    lua bindings test success + resources relocation
    git-svn-id: https://marko-pc:3481/svn/Engine/trunk@16 cf8d60cf-3bd3-6f4a-91c3-f336ec15cb7d

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -1,8 +1,10 @@
 #include "Engine.h"
+#include <iostream>
 
 int Engine::run() {
 	_luaInterpreter = luaL_newstate();
 	luaBind(_luaInterpreter);
+	luaL_dofile(_luaInterpreter, "llua.lua");
 	_renderer = new Renderer();
 	_renderer->init();
 	//this should be called from a loop when we make one
